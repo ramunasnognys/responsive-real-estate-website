bf21561f24df89abccb4361c4692f46089379b9b
Ramunas <dev.nognys@gmail.com>
2022-07-21 08:46:30 +0200

    update menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import '/assets/css/style.css'
 
+console.log("labaaaaa");
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
@@ -54,8 +56,25 @@ const toggleItem = (item) => {
 }
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+  const scrollY = window.pageYOffset
 
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    } 
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
